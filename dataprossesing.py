f58a76d0fb0561c3420bdd2e2ae20d1113d71df1
Elena Kane <elena_kane@hotmail.com>
2023-05-28 11:53:16 +0200

    data

--- dataprossesing.py
+++ dataprossesing.py
@@ -17,14 +17,17 @@ data_list = [v for k, v in data.items()]
 # Create a DataFrame from the list
 df2 = pd.DataFrame(data_list)
 
-# Merge the datasets on the "song" column
+# Merge the datasets on the "Song" column
 merged_df = pd.merge(df1, df2, left_on='Song', right_on='Song', how='inner')
 
 # Check if 'Lyrics translation' is "English", if so, replace it with the value in 'Lyrics'
 merged_df.loc[merged_df['Lyrics translation'] == 'English', 'Lyrics translation'] = merged_df['Lyrics']
 
 # Rename the "Lyrics translation" column to "lyrics"
-merged_df.rename(columns={'Lyrics translation': 'lyrics'}, inplace=True)
+merged_df.rename(columns={'Lyrics translation': 'English Lyrics'}, inplace=True)
+
+# Select only the 'lyrics', 'song', and 'Year' columns
+merged_df = merged_df[['Song', 'English Lyrics']]
 
 # Save the merged dataset to a new CSV file
 merged_df.to_csv('merged.csv', index=False)
